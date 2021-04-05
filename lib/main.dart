c5673ae50d0c9284f060e72cd2b14fdf2e26037d
kings-israel <milimokings@gmail.com>
2021-04-05 13:17:06 +0300

    Slimy Card App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:slim_card/slimy_card_demo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SlimyCardDemo(),
+    );
+  }
+}
+
---
+++ lib/slimy_card_demo.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:slimy_card/slimy_card.dart';
+
+class SlimyCardDemo extends StatefulWidget {
+  @override
+  _SlimyCardDemoState createState() => _SlimyCardDemoState();
+}
+
+class _SlimyCardDemoState extends State<SlimyCardDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      initialData: false,
+      stream: slimyCard.stream,
+      builder: ((context, AsyncSnapshot snapshot) {
+        return ListView(
+            padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(height: 70,),
+            SlimyCard(
+              color: Colors.indigo[300],
+              topCardWidget: topCardWidget((snapshot.data) ? 'images/devs.jpg' : 'images/flutter.png'),
+              bottomCardWidget: bottomCardWidget()
+            )
+          ],
+        );
+      }),
+    );
+  }
+}
+
+Widget topCardWidget(String imagePath) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        height: 70,
+        width: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(image: AssetImage(imagePath)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 20,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+      ),
+      SizedBox(height: 15,),
+      Text(
+        'Flutter',
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+      SizedBox(height: 15,),
+      Center(
+        child: Text(
+          'Flutter is Google\'s UI toolkit for building beautiful, natively compiled applications for mobile, web and desktop from a single codebase',
+          style: TextStyle(
+            color: Colors.white.withOpacity(0.8),
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      SizedBox(height: 10,)
+    ],
+  );
+}
+
+Widget bottomCardWidget() {
+  return Column(
+    children: [
+      Text(
+        'https://flutterdevs.com/',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      SizedBox(height: 15),
+      Expanded(
+        child: Text(
+          'FlutterDevs specializes in creating cost-effective and efficient '
+              'applications with our perfectly crafted,creative and leading-edge '
+              'flutter app development solutions for customers all around the globe.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    ],
+  );
+}
